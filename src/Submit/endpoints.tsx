8d4a9bdbe0890ff0c84e33e6e3a8042d8976feb1
mursyidutopia <mursyid.utopia@gmail.com>
2025-05-05 14:08:49 +0800

    amend cont

--- src/Submit/endpoints.tsx
+++ src/Submit/endpoints.tsx
@@ -1,5 +1,5 @@
 export const ENDPOINTS = {
   SUBMIT_CLAIM: `https://script.google.com/macros/s/AKfycbxn3k5YII8_ZelryzRlv0Sr_wlgcX5G2vqb-LDk4vhKrAkA35k8_hkG8JmeCcp29ft2/exec`,
   SUBMIT_USERINFO: `https://script.google.com/macros/s/AKfycbxn3k5YII8_ZelryzRlv0Sr_wlgcX5G2vqb-LDk4vhKrAkA35k8_hkG8JmeCcp29ft2/exec`,
-  SUBMIT_INCIDENT: `https://script.google.com/macros/s/AKfycbxqIM8ytgNNINLivdrPIkFbHCjh6mhAyS7hAU-oDMWqU4xw0XchGFTwUmGNH6b8xofn/exec`
+  SUBMIT_INCIDENT: `https://script.google.com/macros/s/AKfycbzbxGXaO5jvmM5g1Z5eOWMymmYJ9_xjocdiJlEs9p8RLv2AJJzyNSNP0MzhcK5HRvml/exec`
 };
