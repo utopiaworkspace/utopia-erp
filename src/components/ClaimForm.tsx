271fc6eea6b7f6954c85a200c151cc50f69d7fce
Utopia Workspace <utopiaworkspace33@gmail.com>
2025-08-12 16:47:41 +0800

    3 new company

--- src/components/ClaimForm.tsx
+++ src/components/ClaimForm.tsx
@@ -121,8 +121,8 @@ export default function ClaimForm({
           "MERRY ELDERLY CARE SDN BHD",
           "COLD TRUCK MALAYSIA SDN BHD",
           "MOBILE WHEELER SDN BHD",
-          "ENCIK SKYLIFT & CRANE SDN BHD"
-          "DON'T SELECT - FOR TESTING PURPOSES"
+          "ENCIK SKYLIFT & CRANE SDN BHD"，
+          "⚠️ DON'T SELECT - FOR TESTING PURPOSES"
         ].map((unit) => (
           <MenuItem key={unit} value={unit}>
             {unit}
